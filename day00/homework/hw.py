4f88b598f8348c35c248ad71d3b45eda22a19c7b
goawork <sopiagorgadze@gmail.com>
2025-01-27 17:04:43 +0400

    first commit

---
+++ day00/homework/hw.py
@@ -0,0 +1,23 @@
+# მოცემული 3 მთელი a, b, c ̸= 0 რიცხვებისთვის მოძებნეთ a და
+# b რიცხვებს შორის მოხვედრილი c რიცხვის ჯერადი რიცხვების
+# რაოდენობა ყველა შესაძლო ვარიანტისთვის. თუ რომელიმე
+# a, b-დან ჯერადია c-ს, მაშინ ჩათვალეთ შესაბამისი საზღვარი.
+# მოიფიქრეთ ამოცანის ამოხსნის ალგორითმი, დაწერეთ და გა-
+# უშვით პროგრამა ციკლის და რეკურსიის (აგრეთვე range) კონ-
+# სტრუქციის გამოყენების გარეშე.
+
+
+def count_multiples(a, b, c):
+    if c == 0:
+        return 0 
+    if a <= b:
+        start = a
+        end = b
+    else:
+        start = b
+        end = a
+    multiples_up_to_end = end // c
+    multiples_up_to_start = (start - 1) // c
+    count = multiples_up_to_end - multiples_up_to_start
+    return count
+
